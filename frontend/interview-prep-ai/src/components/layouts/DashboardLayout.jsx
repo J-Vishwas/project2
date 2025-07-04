0b83e8dc841a303ae8e1a14f803ff34f1e710056
J-Vishwas <vishwas96520@gmail.com>
2025-07-04 16:04:38 +0530

    navabar

--- frontend/interview-prep-ai/src/components/layouts/DashboardLayout.jsx
+++ frontend/interview-prep-ai/src/components/layouts/DashboardLayout.jsx
@@ -1,7 +1,7 @@
 import React,{useContext} from "react";
 
 import { UserContext } from "../../context/userContext";
-import NavBar from "./NavBar";
+import Navbar from "./Navbar";
 
 const DashboardLayout = ({ children }) => {
  const {user} = useContext(UserContext);
@@ -10,7 +10,7 @@ const DashboardLayout = ({ children }) => {
   return (
     
       <div>
-        <NavBar/>
+        <Navbar/>
         {user && <div>{children}</div>}
       </div>
   );
